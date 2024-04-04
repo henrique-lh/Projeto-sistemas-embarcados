2af832f0678c43ec40164b46a18f2bdcb8450ef3
João Victor Negreiros <joaovictornsv@gmail.com>
2024-04-04 17:24:39 -0300

    Update sensor_imu.h

--- projeto-embarcados-mpu6050/components/sensor_imu/include/sensor_imu.h
+++ projeto-embarcados-mpu6050/components/sensor_imu/include/sensor_imu.h
@@ -1,1 +1,10 @@
+struct AccelerationData {};
+struct GyroscopeData {};
+
+struct esp_err_t {};
+
+esp_err_t imu_init();
+esp_err_t get_acceleration_data(AccelerationData *data);
+esp_err_t get_gyroscope_data(GyroscopeData *data);
+
 void func(void);
