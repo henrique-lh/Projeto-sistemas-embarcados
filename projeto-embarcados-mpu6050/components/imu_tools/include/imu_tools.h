de1cc191e6f75677dd5de7d73274a2e139ec0ce2
João Victor Negreiros <joaovictornsv@gmail.com>
2024-04-04 17:23:52 -0300

    Update imu_tools.h

--- projeto-embarcados-mpu6050/components/imu_tools/include/imu_tools.h
+++ projeto-embarcados-mpu6050/components/imu_tools/include/imu_tools.h
@@ -1,1 +1,13 @@
+struct IMUData {};
+struct Quaternion {};
+struct EulerAngle {};
+
+struct esp_err_t {};
+
+esp_err_t get_imu_data(IMUData *data);
+esp_err_t calculate_quaternion(const IMUData *data, Quaternion *quaternion);
+esp_err_t get_euler_angles(const Quaternion *quaternion, EulerAngle *euler);
+esp_err_t quaternion_to_euler(const Quaternion *quaternion, EulerAngle *euler);
+esp_err_t get_quaternion(Quaternion *quaternion);
+
 void func(void);
