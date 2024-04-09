d7c276032d0e048f3445076eb5afba8e42e09312
Luis Henrique <henrique.santos.lhls@gmail.com>
2024-04-08 22:05:52 -0300

    add part of implementation

--- projeto-embarcados-mpu6050/components/imu_tools/imu_tools.c
+++ projeto-embarcados-mpu6050/components/imu_tools/imu_tools.c
@@ -1,7 +1,82 @@
 #include <stdio.h>
 #include "imu_tools.h"
 
-void func(void)
-{
+esp_err_t get_imu_data(IMUData *data) {
 
+    // salvar dados do data
+
+    ESP_LOGD(tag, ">> mpu6050");
+	i2c_config_t conf;
+	conf.mode = I2C_MODE_MASTER;
+	conf.sda_io_num = PIN_SDA;
+	conf.scl_io_num = PIN_CLK;
+	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.clk_flags = 0;
+	conf.master.clk_speed = 100000;
+	ESP_ERROR_CHECK(i2c_param_config(I2C_NUM_0, &conf));
+	ESP_ERROR_CHECK(i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0));
+
+	i2c_cmd_handle_t cmd;
+	vTaskDelay(200/portTICK_PERIOD_MS);
+
+	cmd = i2c_cmd_link_create();
+	ESP_ERROR_CHECK(i2c_master_start(cmd));
+	ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (I2C_ADDRESS << 1) | I2C_MASTER_WRITE, 1));
+	i2c_master_write_byte(cmd, MPU6050_ACCEL_XOUT_H, 1);
+	ESP_ERROR_CHECK(i2c_master_stop(cmd));
+	i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000/portTICK_PERIOD_MS);
+	i2c_cmd_link_delete(cmd);
+
+	cmd = i2c_cmd_link_create();
+	ESP_ERROR_CHECK(i2c_master_start(cmd));
+	ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (I2C_ADDRESS << 1) | I2C_MASTER_WRITE, 1));
+	i2c_master_write_byte(cmd, MPU6050_PWR_MGMT_1, 1);
+	i2c_master_write_byte(cmd, 0, 1);
+	ESP_ERROR_CHECK(i2c_master_stop(cmd));
+	i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000/portTICK_PERIOD_MS);
+	i2c_cmd_link_delete(cmd);
+
+
+	uint8_t data[14];
+
+	float accel_x;
+	float accel_y;
+	float accel_z;
+
+	while(1) {
+		cmd = i2c_cmd_link_create();
+		ESP_ERROR_CHECK(i2c_master_start(cmd));
+		ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (I2C_ADDRESS << 1) | I2C_MASTER_WRITE, 1));
+		ESP_ERROR_CHECK(i2c_master_write_byte(cmd, MPU6050_ACCEL_XOUT_H, 1));
+		ESP_ERROR_CHECK(i2c_master_stop(cmd));
+		ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000/portTICK_PERIOD_MS));
+		i2c_cmd_link_delete(cmd);
+
+		cmd = i2c_cmd_link_create();
+		ESP_ERROR_CHECK(i2c_master_start(cmd));
+		ESP_ERROR_CHECK(i2c_master_write_byte(cmd, (I2C_ADDRESS << 1) | I2C_MASTER_READ, 1));
+
+		ESP_ERROR_CHECK(i2c_master_read_byte(cmd, data,   0));
+		ESP_ERROR_CHECK(i2c_master_read_byte(cmd, data+1, 0));
+		ESP_ERROR_CHECK(i2c_master_read_byte(cmd, data+2, 0));
+		ESP_ERROR_CHECK(i2c_master_read_byte(cmd, data+3, 0));
+		ESP_ERROR_CHECK(i2c_master_read_byte(cmd, data+4, 0));
+		ESP_ERROR_CHECK(i2c_master_read_byte(cmd, data+5, 1));
+
+		//i2c_master_read(cmd, data, sizeof(data), 1);
+		ESP_ERROR_CHECK(i2c_master_stop(cmd));
+		ESP_ERROR_CHECK(i2c_master_cmd_begin(I2C_NUM_0, cmd, 1000/portTICK_PERIOD_MS));
+		i2c_cmd_link_delete(cmd);
+
+		accel_x = byteToMs2((data[0] << 8) | data[1]);
+		accel_y = byteToMs2((data[2] << 8) | data[3]);
+		accel_z = byteToMs2((data[4] << 8) | data[5]);
+        
+        // test get data
+        // printf(">> accel_x: %f, accel_y: %f, accel_z: %f\n", accel_x, accel_y, accel_z);
+		vTaskDelay(500/portTICK_PERIOD_MS);
+	}
+
+	vTaskDelete(NULL);
 }
